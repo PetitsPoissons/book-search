caa3379f2b7cb15bb690ff33b7d89b26c8fde601
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-10-10 15:20:50 -0700

    authentication

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,17 @@
 // this file stores all of the GraphQL query requests
 import gql from 'graphql-tag';
 
-export const GET_ME = gql``;
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
